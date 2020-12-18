a494818d12d4e926c4b7dd7d46d40d01ece9cd2c
Perminus-prime <64547801+Perminus-prime@users.noreply.github.com>
2020-12-18 20:46:55 +0300

    getting everything ready

---
+++ createJobs.groovy
@@ -0,0 +1,10 @@
+// uses jenkins jobs dsl syntax
+
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
